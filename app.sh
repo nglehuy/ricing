e87461aa495732a659a51a61ef8da5202026be1c
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-04-05 01:18:09 +0700

    Update

--- app.sh
+++ app.sh
@@ -3,13 +3,13 @@ cd $HOME
 
 echo "Installing apps ..."
 sudo pacman -Syu --noconfirm --needed discord chromium snapd dunst \
-  network-manager-applet thunar ranger kitty feh okular xcursor-breeze \
+  network-manager-applet thunar ranger kitty feh zathura xcursor-breeze \
   blueman ibus ibus-hangul yad yay ttf-font-awesome rofi polybar picom \
   gnome-disk-utility bspwm sxhkd nm-connection-editor python-pywal \
   noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji \
   code
 
-yay -Syu slack-desktop shutter perl-goo-canvas ibus-bamboo \
+yay -Syu --needed slack-desktop shutter perl-goo-canvas ibus-bamboo \
   anydesk-bin themix-full-git otf-code-new-roman ttf-quicksand
 
 echo "Setting up snapd ..."
