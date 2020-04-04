c9959fea18b6d92c7d1216b75485525a2831fc12
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-04-05 01:07:49 +0700

    Update

--- app.sh
+++ app.sh
@@ -6,8 +6,12 @@ sudo pacman -Syu --noconfirm --needed discord chromium snapd dunst \
   network-manager-applet thunar ranger kitty feh okular xcursor-breeze \
   blueman ibus ibus-hangul yad yay ttf-font-awesome rofi polybar picom \
   gnome-disk-utility bspwm sxhkd nm-connection-editor python-pywal \
-  noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji
+  noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji \
+  code
 
 yay -Syu slack-desktop shutter perl-goo-canvas ibus-bamboo \
-  anydesk-bin themix-full-git
+  anydesk-bin themix-full-git otf-code-new-roman ttf-quicksand
 
+echo "Setting up snapd ..."
+sudo systemctl enable --now snapd.socket
+sudo ln -s /var/lib/snapd/snap /snap
