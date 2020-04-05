3d7fcba5f2cf261691786a4c8df15412e684c06c
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-04-05 16:55:43 +0700

    Update

--- setup.sh
+++ setup.sh
@@ -6,11 +6,14 @@ cp -l -f -T -r $PWD/home $HOME
 echo "Linking .config directory ..."
 cp -l -f -T -r $PWD/config $XDG_CONFIG_HOME
 
+echo "Linking /root/.config directory ..."
+sudo cp -l -f -T -r $PWD/config /root/.config
+
 echo "Linking /etc directory ..."
 sudo cp -l -f -T -r $PWD/etc /etc
 
 echo "Copying themes ..."
-sudo cp -rT $PWD/themes /usr/share/themes
+sudo cp -rfT $PWD/themes /usr/share/themes
 
 echo "Copying backgrounds ..."
-sudo cp -rT $PWD/backgrounds /usr/share/backgrounds
+sudo cp -rfT $PWD/backgrounds /usr/share/backgrounds
