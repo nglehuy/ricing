6c12028a3affc357806e6a40d6faf89f81181a28
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-04-06 20:01:17 +0700

    Update

--- app.sh
+++ app.sh
@@ -1,22 +1,26 @@
 #!/usr/bin/env sh
 cd $HOME
 
-echo "Installing desktop tools ..."
+sudo pacman -Rs --noconfirm manjaro-hello
+sudo pacman -Rs $(pacman -Qnq | grep manjaro-gnome*)
+sudo pacman -Rs $(pacman -Qnq | grep gnome*)
+sudo pacman -Rs $(pacman -Qnq | grep maia)
+
+echo "Installing desktop apps ..."
 sudo pacman -Syu --noconfirm --needed  dunst \
   network-manager-applet nm-connection-editor networkmanager \
   light \
   ranger \
   feh \
-  zathura polkit-gnome \
+  zathura polkit-gnome gnome \
   xcursor-breeze papirus-icon-theme \
   blueman xorg-xsetroot \
   ibus ibus-hangul \
   ttf-font-awesome noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji \
+  ttf-roboto \
   yad rofi polybar picom bspwm bspwm-scripts sxhkd python-pywal \
-  gnome-disk-utility yay
-
-echo "Installing desktop apps ..."
-sudo pacman -Syu --noconfirm --needed discord chromium snapd code docker kitty
+  gnome-disk-utility yay \
+  discord chromium snapd code docker kitty
 
 echo "Setting up snapd ..."
 sudo systemctl enable --now snapd.socket
@@ -27,3 +31,5 @@ sudo usermod -a -G docker $USER
 
 echo "Add user to video group ..."
 sudo usermod -a -G video $USER
+
+rm -rf $HOME/.local/share/keyrings/*
