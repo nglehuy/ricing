219c102edd9bef93938a93a456a9082523144eb4
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-08-22 00:03:40 +0700

    update

--- setup.sh
+++ setup.sh
@@ -1,5 +1,26 @@
 #!/usr/bin/env sh
 
+echo "Installing apps ..."
+sudo pacman -Syu --needed --noconfirm dunst pamac \
+  network-manager-applet nm-connection-editor networkmanager \
+  light dex \
+  ranger \
+  feh \
+  zathura polkit-gnome gnome gnome-screensaver \
+  papirus-icon-theme \
+  blueman xorg-xsetroot \
+  ibus ibus-hangul \
+  ttf-font-awesome noto-fonts noto-fonts-extra noto-fonts-cjk noto-fonts-emoji \
+  yad rofi polybar picom bspwm-scripts sxhkd python-pywal \
+  gnome-disk-utility \
+  discord kitty
+
+yay -Syu --needed --noconfirm slack-desktop shutter perl-goo-canvas ibus-bamboo \
+  nerd-fonts-fira-code radeontop oomox 
+
+echo "Add user to video group ..."
+sudo usermod -a -G video $USER
+
 echo "Linking home directory ..."
 cp -l -f -T -r $PWD/home $HOME
 
@@ -19,4 +40,3 @@ echo "Copying backgrounds ..."
 sudo cp -rfT $PWD/backgrounds /usr/share/backgrounds
 
 wal --theme base16-onedark
-fc-cache -fv
--- yay.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env sh 
-
-yay -Syu --needed slack-desktop shutter perl-goo-canvas ibus-bamboo \
-  nerd-fonts-fira-code radeontop oomox 
