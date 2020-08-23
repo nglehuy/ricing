28206c3fbb46618b2f5019eb693eeeda9e3b30e8
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-08-23 15:02:55 +0700

    update

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,13 +6,17 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar
-if type "xrandr"; then
-  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
-    MONITOR=$m polybar -c $HOME/.config/polybar/config -r panel 2> /tmp/polybar_log &
-  done
-else
-  polybar -c $HOME/.config/polybar/config -r panel 2> /tmp/polybar_log &
-fi
+CONFIG=$HOME/.config/polybar/config
+LOG=/tmp/polybar_log
+
+PRIMARY=$(xrandr --query | grep " connected" | grep "primary" | cut -d" " -f1)
+OTHERS=$(xrandr --query | grep " connected" | grep -v "primary" | cut -d" " -f1)
+
+MONITOR=$PRIMARY polybar -c $CONFIG -r panel 2> $LOG &
+sleep 1
+
+for m in $OTHERS; do
+  MONITOR=$m polybar -c $CONFIG -r subpanel 2> $LOG &
+done
 
 echo "Bars launched..."
