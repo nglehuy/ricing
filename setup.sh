d8f6abcb0fe348ed17c3a6a1a0730b143a1a02ce
Huy Le Nguyen <nlhuy.cs.16@gmail.com>
2020-09-09 15:43:54 +0700

    update

--- setup.sh
+++ setup.sh
@@ -1,12 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "Installing apps ..."
 sudo pacman -Syu --needed --noconfirm dunst \
   network-manager-applet nm-connection-editor networkmanager \
   light dex \
-  ranger \
-  feh \
-  zathura polkit-gnome gnome gnome-screensaver \
+  nitrogen \
+  zathura polkit-gnome lightdm lightdm-pantheon-greeter light-locker \
   papirus-icon-theme \
   blueman xorg-xsetroot \
   ibus ibus-hangul \
@@ -15,27 +14,25 @@ sudo pacman -Syu --needed --noconfirm dunst \
   gnome-disk-utility \
   discord kitty
 
-yay -Syu --needed --noconfirm slack-desktop ibus-bamboo nerd-fonts-fira-code radeontop oomox polybar
+yay -Syu --needed --noconfirm slack-desktop ibus-bamboo \
+    nerd-fonts-fira-code radeontop oomox polybar layan-cursor-theme-git
 
 echo "Add user to video group ..."
 sudo usermod -a -G video $USER
 
 echo "Linking home directory ..."
-cp -l -f -T -r $PWD/home $HOME
+cp -l -rfT $PWD/home $HOME
 
 echo "Linking .config directory ..."
-cp -l -f -T -r $PWD/config $HOME/.config
+cp -l -rfT $PWD/config $HOME/.config
 
 echo "Linking /root/.config/gtk-3.0 directory ..."
-sudo cp -l -f -T -r $PWD/config/gtk-3.0 /root/.config/gtk-3.0
+sudo cp -l -rfT $PWD/config/gtk-3.0 /root/.config/gtk-3.0
 
 echo "Linking /root/.gtkrc-2.0 directory ..."
-sudo cp -l -f -T -r $PWD/home/.gtkrc-2.0 /root/.gtkrc-2.0
-
-echo "Copying themes ..."
-sudo cp -rfT $PWD/themes /usr/share/themes
+sudo cp -l -rfT $PWD/home/.gtkrc-2.0 /root/.gtkrc-2.0
 
 echo "Copying backgrounds ..."
 sudo cp -rfT $PWD/backgrounds /usr/share/backgrounds
 
-wal --theme base16-onedark
+wal --theme nord
